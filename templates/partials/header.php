38a43b1f4de6d73779a2df227a126ac1ea7b406f
JustInTimez <just.oost@gmail.com>
2022-11-07 18:18:37 +0200

    More structure
    Created and included base files for header and footer. Started with mapping out database information as well

---
+++ templates/partials/header.php
@@ -0,0 +1,33 @@
+<?php
+include __DIR__ . "/head.php"
+?>
+
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3 text-center">
+        <a class="navbar-brand">Hotel Bookings Western Cape</a>
+        <button class="navbar-toggler text-center" type="button" data-bs-target="#navCollapse" data-bs-toggle="collapse" aria-controls="navCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navCollapse">
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <!-- Remember to change/update hrefs when/if needed -->
+                    <a class="nav-link" href="#Placeholder">Placeholder</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Placeholder">Placeholder</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#Placeholder">Placeholder</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Placeholder">Placeholder</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <!-- Navbar END -->
+</header>
