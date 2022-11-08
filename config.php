08e6e2f8e0363b7a1973fd660a64d809a4e6bf83
JustInTimez <just.oost@gmail.com>
2022-11-08 11:48:03 +0200

    Connection message updated
    Changed connection message to indicate which db it is connected to, to make sure!

--- config.php
+++ config.php
@@ -11,4 +11,4 @@ $connect = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connect) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully. WOOHOO!";
+echo "Connected successfully to DB named " . $dbname;
--- index.php
+++ index.php
@@ -15,23 +15,31 @@
         <div id="content-wrap">
             <h1 class="text-center">Register or Login</h1>
             <div class="container text-center">
-                <div class="row row-cols-2">
-                    <div class="col flex">Register:
+                <div class="row">
+                    <div class="col-lg-6">Register:
                         <form action="process.php" method="post">
                             <div class="mb-3">
+                                <label for="InputName" class="form-label">Name</label>
+                                <input type="text" name="InputName" class="form-control" id="InputName">
+                            </div>
+                            <div class="mb-3">
+                                <label for="InputSurname" class="form-label">Surname</label>
+                                <input type="text" name="InputSurname" class="form-control" id="InputSurname">
+                            </div>
+                            <div class="mb-3">
                                 <label for="InputEmail" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
+                                <input type="email" name="InputEmail" class="form-control" id="InputEmail">
                             </div>
                             <div class="mb-3">
                                 <label for="InputPassword" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="InputPassword">
+                                <input type="password" name="InputPassword" class="form-control" id="InputPassword">
                             </div>
-                            <button type="submit" class="btn btn-dark">Submit</button>
+                            <button type="submit" name='Submit' class="btn btn-dark">Submit</button>
                         </form>
 
                     </div>
 
-                    <div class="col">Customer/Staff Login:
+                    <div class="col-lg-6">Customer/Staff Login:
                         <form action="process.php" method="post">
                             <div class="mb-3">
                                 <label for="InputEmail" class="form-label">Email address</label>
