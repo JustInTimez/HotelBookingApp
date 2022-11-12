d7832bf79e94480c03574cd654b6cfddcde0b08e
JustInTimez <just.oost@gmail.com>
2022-11-12 17:00:08 +0200

    Facility icons and display logic
    Adding more hotel information to the homepage for users to see and compare

--- home.php
+++ home.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-require_once __DIR__ . "/config.php"; 
+require_once __DIR__ . "/config.php";
 include_once __DIR__ . "./templates/partials/head.php";
 include_once __DIR__ . "./templates/partials/header.php";
 
-$sql = "SELECT name, image, address, price, capacity, description FROM hotels";
-$result = $connect->query($sql);         // Storing hotel info in a variable
+$sql = "SELECT name, image, address, price, capacity, description, has_wifi, has_ac, has_parking, is_petfriend, has_pool, is_accessible, rating FROM hotels";
+
+// Storing hotel info in a variable
+$result = $connect->query($sql);
 
 // Close connection
 mysqli_close($connect);
@@ -28,23 +30,46 @@ mysqli_close($connect);
         <!-- Cards: Hotels -->
         <div class="row row-cols-1 row-cols-lg-3 g-5 m-0">
             <?php
-                while($row = $result->fetch_assoc()) {
-                    echo '
-                        <div class="col">
+            while ($row = $result->fetch_assoc()) {
+                $facilities = [
+                'has_wifi' => ['show' => $row['has_wifi'], 'icon' => 'wifi-solid.svg'], 
+                'has_ac' => ['show' => $row['has_ac'], 'icon' => 'snowflake-solid.svg'], 
+                'has_parking' => ['show' => $row['has_parking'], 'icon' => 'square-parking-solid.svg'], 
+                'is_petfriend' =>  ['show' => $row['is_petfriend'], 'icon' => 'paw-solid.svg'], 
+                'has_pool' =>  ['show' => $row['has_pool'], 'icon' => 'water-ladder-solid.svg'], 
+                'is_accessible' =>  ['show' => $row['is_accessible'], 'icon' => 'wheelchair-solid.svg']];
+
+                $facilityIcons = "";
+                foreach ($facilities as $key => $value) {
+                    if ($value['show'] == 1){
+                      $facilityIcons .= '<img class="me-3" src="./static/images/hotels/facility-icons/' . $value["icon"] . '">'; 
+                    }
+
+                }
+                echo '
+                        <div class="col-12 col-xl-6 col-md-6">
                             <div class="card border-dark bg-dark text-white shadow h-100">
                                 <img src="./static/images/hotels/' . $row["image"] . '" height="270" class="card-img-top hotel-image" alt="">
-                                <div class="card-body d-flex flex-column">
-                                    <h5 class="card-title">' . $row["name"] . ' </h5>
-                                    <p class="card-text small">Facilities
-                                    </p>
-                                    <div class="text-center mt-5">
-                                        <a href="" target="_blank" class="btn btn-light mt-auto">Book</a>
-                                        <a href="" target="_blank" class="btn btn-light mt-auto">Details</a>
+                                <div class="card-body">
+                                <h5 class="card-title">' . $row["name"] . ' </h5>
+                                    <div class="d-flex" >
+                                        <div class="d-flex flex-column">
+                                            <p class="card-text small">Facilities</p>
+                                            <div> ' . $facilityIcons . ' </div>
+                                            
+                                            <div class="mt-5">
+                                                <a href="" target="_blank" class="btn btn-light mt-auto">Book</a>
+                                                <a href="" target="_blank" class="btn btn-light mt-auto">Details</a>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex flex-column align-items-end flex-fill justify-content-end">
+                                        <p class="display-5 lh-1 mb-1">R '. $row["price"] . '</p><span class="small mb-0">per night (sleeps: '. $row["capacity"] . ')</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>';
-                }
+            }
             ?>
         </div>
     </main>
